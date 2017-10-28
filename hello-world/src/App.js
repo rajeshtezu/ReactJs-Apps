d1c6ae10135c0f8ac322f3e3da869142a3a8609c
Rajesh Kumar <rajeshtezu90@gmail.com>
2017-10-28 19:05:46 +0530

    Separated Header and Intro components in individual files

--- hello-world/src/App.js
+++ hello-world/src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+import Header from './Header';
+import Intro from './Intro';
 
 class App extends Component {
   render() {
@@ -13,29 +15,5 @@ class App extends Component {
   }
 }
 
-class Header extends Component {
-  render(){
-    return(
-      <div>
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-      </div>
-    );
-  }
-}
-
-class Intro extends Component {
-  render(){
-    return(
-      <div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
-}
 
 export default App;
