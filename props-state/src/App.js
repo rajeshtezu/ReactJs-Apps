989747c2a0d7bc90ef5153c48fd8b7b8932245e7
Rajesh Kumar <rajeshtezu90@gmail.com>
2017-10-29 09:42:21 +0530

    installed prop-types module and created few props

--- props-state/src/App.js
+++ props-state/src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import logo from './logo.svg';
 import './App.css';
 
@@ -14,9 +15,29 @@ class App extends Component {
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
 
+        <h3> prop number is: {this.props.propNumber} </h3>
+        <h3> prop string is: {this.props.propString} </h3>
+        <h3> prop object 1 is: {this.props.propObject.obj1} </h3>
+
         <Parent />
       </div>
     );
+  }
+}
+
+App.propTypes = {
+  propObject: PropTypes.object,
+  propString: PropTypes.string,
+  propNumber: PropTypes.number
+}
+
+App.defaultProps = {
+  propNumber: 3,
+  propString: "This is a prop string.",
+  propObject: {
+    obj1: "I am obj 1",
+    obj2: "I am obj 2",
+    obj3: "I am obj 3"
   }
 }
 
