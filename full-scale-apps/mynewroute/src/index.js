17df1a34df6b62c34f62dc0e04a637379c0538cf
Rajesh Kumar <rajeshtezu90@gmail.com>
2017-11-01 09:11:31 +0530

    Initial commit. working with React Router

---
+++ full-scale-apps/mynewroute/src/index.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
+
+import Home from './Home';
+import Contact from './Contact';
+
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom';
+
+ReactDOM.render(<Router>
+  <div>
+    <ul>
+      <li><Link to="/">Home</Link></li>
+      <li><Link to="/contact">Contact-me</Link></li>
+    </ul>
+
+    <hr />
+    <Route exact path="/" component={Home} />
+    <Route path="/contact" component={Contact} />
+  </div>
+  </Router>
+  , document.getElementById('root'));
+registerServiceWorker();
