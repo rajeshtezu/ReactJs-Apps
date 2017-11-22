d95de6ff60db94f90bdf777683e5a0e8dca37373
Rajesh Kumar <rajeshtezu90@gmail.com>
2017-11-22 20:00:29 +0530

    Added questions

--- full-scale-apps/u-survey/src/App.js
+++ full-scale-apps/u-survey/src/App.js
@@ -11,7 +11,6 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-
         <Usurvey />
       </div>
     );
--- full-scale-apps/u-survey/src/Usurvey.js
+++ full-scale-apps/u-survey/src/Usurvey.js
@@ -14,12 +14,27 @@ var config = {
 firebase.initializeApp(config);
 
 class Usurvey extends Component {
+  nameSubmit(event){
+    let studentName = this.refs.name.value;
+    this.setState({studentName: studentName}, function(){
+      console.log(this.state)
+    });
+  }
+
+  answerSelected(){
+
+  }
+
+  questionSubmit(){
+    
+  }
+
   constructor(props){
     super(props);
 
     this.state = {
       uid: uuid.v1(),
-      studentName: '',
+      studentName: 'something',
       answers: {
         answer1: '',
         answer2: '',
@@ -27,6 +42,9 @@ class Usurvey extends Component {
       },
       isSubmitted: false
     };
+    this.nameSubmit = this.nameSubmit.bind(this);
+    this.answerSelected = this.answerSelected.bind(this);
+    this.questionSubmit = this.questionSubmit.bind(this);
   }
 
   render(){
@@ -36,16 +54,45 @@ class Usurvey extends Component {
     if(this.state.studentName === '' && this.state.isSubmitted === false){
       studentName = <div>
         <h1>Your name please...</h1>
-        <form>
-          <input type="text" placeholder="Enter your name" ref="name" />
+        <form onSubmit={this.nameSubmit}>
+          <input className="namy" type="text" placeholder="Enter your name" ref="name" />
         </form>
-      </div>
+      </div>;
+
+      questions = '';
+    }
+    else if(this.state.studentName !== '' && this.state.isSubmitted === false){
+      studentName = <h1>Welcome to U-survey, {this.state.studentName}</h1>;
+      questions = <div>
+        <h2>Here are some questions: </h2>
+        <form onSubmit={this.questionSubmit}>
+          <div className="card">
+            <label>What kind of courses you like the most: </label> <br />
+            <input type="radio" name="answer1" value="Technology" onChange={this.answerSelected}/>Technology
+            <input type="radio" name="answer1" value="Design" onChange={this.answerSelected}/>Design
+            <input type="radio" name="answer1" value="Marketing" onChange={this.answerSelected}/>Marketing
+          </div>
+          <div className="card">
+            <label>You are a: </label> <br />
+            <input type="radio" name="answer2" value="Student" onChange={this.answerSelected}/>Student
+            <input type="radio" name="answer2" value="in-job" onChange={this.answerSelected}/>in-job
+            <input type="radio" name="answer2" value="looking-job" onChange={this.answerSelected}/>looking-job
+          </div>
+          <div className="card">
+            <label>Is online courses helpful: </label> <br />
+            <input type="radio" name="answer3" value="Yes" onChange={this.answerSelected}/>Yes
+            <input type="radio" name="answer3" value="No" onChange={this.answerSelected}/>No
+            <input type="radio" name="answer3" value="maybe" onChange={this.answerSelected}/>Maybe
+          </div>
+          <input className="feedback-button" type="submit" value="Submit" />
+        </form>
+      </div>;
     }
 
     return(
       <div>
         {studentName}
-        -------------------------------------
+        --------------------------------------------------
         {questions}
       </div>
     );
