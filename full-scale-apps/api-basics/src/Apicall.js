082d62addb96de1206b8fb94cd6907a33defd174
Rajesh Kumar <rajeshtezu90@gmail.com>
2017-12-02 08:42:26 +0530

    Initial commit. using api fetcher axios

---
+++ full-scale-apps/api-basics/src/Apicall.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+
+class Apicall extends Component {
+  render(){
+    return(
+      <div>
+        I am from Apicall.
+      </div>
+    );
+  }
+}
+
+export default Apicall;
